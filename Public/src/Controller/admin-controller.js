93196c4ada29ab96c0b4566369d3d3a254300c7f
Abhinay yadav <abhinayy206@gmail.com>
2026-08-04 20:51:47 +0530

    Refactor admin controller: remove commented code, add refresh token handling, and implement account update and password change functionalities

--- Public/src/Controller/admin-controller.js
+++ Public/src/Controller/admin-controller.js
@@ -5,13 +5,6 @@ import { Admin } from "../models/admin.model.js";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
-// const Admin = require('../models/adminSchema.js');
-// const Sclass = require('../models/sclassSchema.js');
-// const Student = require('../models/studentSchema.js');
-// const Teacher = require('../models/teacherSchema.js');
-// const Subject = require('../models/subjectSchema.js');
-// const Notice = require('../models/noticeSchema.js');
-// const Complain = require('../models/complainSchema.js');
 
 const generatejwtToken = async (adminId) => {
     try{
@@ -118,100 +111,99 @@ const logoutadmin = asynchandler ( async (req, res) => {
     .json(new ApiResponse(200, "Admin logged out successfully",))
 })
 
+const refreshAccessToken = asynchandler ( async ( req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-
-// const adminRegister = async ( req, res) => {
-//     try {
-//         const admin = new Admin({
-//             ...req.body
-//         });
-
-//         const existingAdminByEmail = await Admin.findOne({ email: req.body.email });
-//         const existingSchool = await Admin.findOne({ schoolName: req.body.schoolName });
-
-//         if (existingAdminByEmail) {
-//             res.send({ message: 'Email already exists' });
-//         }
-//         else if (existingSchool) {
-//             res.send({ message: 'School name already exists' });
-//         }
-//         else {
-//             let result = await admin.save();
-//             result.password = undefined;
-//             res.send(result);
-//         }
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// };
-
-// const adminLogIn = async (req, res) => {
-//     if (req.body.email && req.body.password) {
-//         let admin = await Admin.findOne({ email: req.body.email });
-//         if (admin) {
-//             if (req.body.password === admin.password) {
-//                 admin.password = undefined;
-//                 res.send(admin);
-//             } else {
-//                 res.send({ message: "Invalid password" });
-//             }
-//         } else {
-//             res.send({ message: "User not found" });
-//         }
-//     } else {
-//         res.send({ message: "Email and password are required" });
-//     }
-// };
-
-const getAdminDetail = async (req, res) => {
-    try {
-        let admin = await Admin.findById(req.params.id);
-        if (admin) {
-            admin.password = undefined;
-            res.send(admin);
-        }
-        else {
-            res.send({ message: "No admin found" });
-        }
-    } catch (err) {
-        res.status(500).json(err);
+    if( !incomingRefreshToken) {
+        throw new ApiError(401, "unauthorized request")
     }
+
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.JWT_REFRESH_SECRET)
+
+        if (!admin) {
+            throw new ApiError(401, "unauthorized request")
+        }
+        if (incomingRefreshToken !== admin.refreshToken) {
+            throw new ApiError(401, "Refresh token is expired")
+        }
+
+        const { accessToken, newRefreshToken } = await generateAccessAndRefreshToken(admin._id)
+
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newRefreshToken, options)
+        .json(new ApiResponse(200, "Access token refreshed successfully", { accessToken, newRefreshToken }))
+    }  
+       catch (error) {
+        throw new ApiError(401, "Invalid refresh token")
+    }
+
+});
+
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+     if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave: false}) 
+
+     return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "User fetched successfully"
+    ))
+})
+
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {fullName, email} = req.body
+
+    if (!fullName || !email) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email: email
+            }
+        },
+        {new: true}
+        
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+})
+
+
+
+
+export {
+    adminRegister,
+    loginadmin,
+    logoutadmin,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails
 }
-
-// const deleteAdmin = async (req, res) => {
-//     try {
-//         const result = await Admin.findByIdAndDelete(req.params.id)
-
-//         await Sclass.deleteMany({ school: req.params.id });
-//         await Student.deleteMany({ school: req.params.id });
-//         await Teacher.deleteMany({ school: req.params.id });
-//         await Subject.deleteMany({ school: req.params.id });
-//         await Notice.deleteMany({ school: req.params.id });
-//         await Complain.deleteMany({ school: req.params.id });
-
-//         res.send(result)
-//     } catch (error) {
-//         res.status(500).json(err);
-//     }
-// }
-
-// const updateAdmin = async (req, res) => {
-//     try {
-//         if (req.body.password) {
-//             const salt = await bcrypt.genSalt(10)
-//             res.body.password = await bcrypt.hash(res.body.password, salt)
-//         }
-//         let result = await Admin.findByIdAndUpdate(req.params.id,
-//             { $set: req.body },
-//             { new: true })
-
-//         result.password = undefined;
-//         res.send(result)
-//     } catch (error) {
-//         res.status(500).json(err);
-//     }
-// }
-
-// module.exports = { adminRegister, adminLogIn, getAdminDetail, deleteAdmin, updateAdmin };
-
-module.exports = { adminRegister, adminLogIn, getAdminDetail };
